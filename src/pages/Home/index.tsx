768b7f2e35a83935951d4bcb8432223b8301b566
Mark Romero <mtovar0@hotmail.com>
2022-12-29 17:03:09 -0500

    primer commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,47 @@
+import { useContext } from "react";
+import { PokeballIconSmall } from "../../assets/pokeball";
+import { Filters } from "../../components/Filters";
+import { Pagination } from "../../components/Pagination";
+import { PokemonList } from "../../components/PokemonList";
+import { PokemonContext } from "../../context/PokemonContext";
+import { usePagination } from "../../hooks/usePagination";
+
+import styles from "./styles.module.scss";
+
+export const Home = () => {
+  const { pokemonsFiltered } = useContext(PokemonContext);
+  const { page, nextPage, previousPage, backToHome } = usePagination();
+
+  let perPage = 12;
+
+  return (
+    <div className={styles.home}>
+      <header>
+        <div onClick={backToHome}>
+          <PokeballIconSmall className='pokeball-icon'/>
+          <span>Pokédex</span>
+        </div>
+      </header>
+      <Pagination
+        page={page}
+        perPage={perPage}
+        nextPage={nextPage}
+        previousPage={previousPage}
+        maxItems={pokemonsFiltered?.length!}
+      />
+      <Filters />
+      <PokemonList
+        page={page}
+        perPage={perPage}
+        pokemonsUrls={pokemonsFiltered}
+      />
+      <Pagination
+        page={page}
+        perPage={perPage}
+        nextPage={nextPage}
+        previousPage={previousPage}
+        maxItems={pokemonsFiltered?.length!}
+      />
+    </div>
+  );
+};
